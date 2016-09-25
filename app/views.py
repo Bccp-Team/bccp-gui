8584916913a0f3fa3ea031aa7f1f5b702e396f91
BRAUGE Corentin <brauge_c@yaka.epita.fr>
2016-09-25 16:03:46 +0200

    views: home: display charts
    * app/templates/index.html: Here.
    * app/views.py: Here.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from flask import render_template
 from app import app
+from app import api
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    stats_batch = api.stats_batch()
+    stats_runners = api.stats_runners()
+    stats_run_global = api.stats_run()
+    return render_template('index.html', stats_batch = stats_batch,
+            stats_runners = stats_runners, stats_run_global = stats_run_global)
